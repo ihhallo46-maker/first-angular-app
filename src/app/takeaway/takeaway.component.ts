d7608879ec875f1066ec9701103c33d99c890a61
richard <ihhallo46@gmail.com>
2026-06-11 21:06:02 +0200

    Json Datei geändert

--- src/app/takeaway/takeaway.component.ts
+++ src/app/takeaway/takeaway.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-takeaway',
+  standalone: true,
   templateUrl: './takeaway.component.html',
   styleUrl: './takeaway.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
